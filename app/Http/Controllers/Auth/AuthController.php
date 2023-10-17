5bd3ae503597ecaa0c7a5f0eea12eb5e3a49792b
naufalamr17 <naufal.amr17@outlook.co.id>
2023-10-17 08:27:01 +0700

    Update: LogOut Functional

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
-use Auth;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
