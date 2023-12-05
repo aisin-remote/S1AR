d5f30a37c10bed9becc029f00c918e7a3e372ec2
naufalamr17 <naufal.amr17@outlook.co.id>
2023-12-05 13:57:48 +0700

    Update: Fix Side Bar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -6,6 +6,54 @@
         <div class="sidebar-brand sidebar-brand-sm">
             <a href="/">S1AR</a>
         </div>
+
+        @php
+        $npk = auth()->user()->npk;
+
+        $userInfo = DB::select(
+        '
+        SELECT TOP 1 attdly2.empno, pnhhira.hirar, MAX(pnhhira.mutdt) AS mutdt, ssmhira.descr
+        FROM attdly2
+        LEFT JOIN pnhhira ON attdly2.empno = pnhhira.empno
+        LEFT JOIN ssmhira ON pnhhira.hirar = ssmhira.hirar
+        WHERE attdly2.empno = ?
+        GROUP BY attdly2.empno, pnhhira.hirar, ssmhira.descr
+        ORDER BY mutdt DESC;
+        ',
+        [$npk]
+        );
+
+        if (!empty($userInfo)) {
+        $npkDesc = $userInfo[0]->hirar; // Use array syntax
+
+        $cleanedString = str_replace(' ', '', $npkDesc);
+
+        // Hitung jumlah karakter
+        $jumlahKarakter = strlen($cleanedString);
+
+        // Tentukan jenis berdasarkan jumlah karakter
+        if ($jumlahKarakter == 5) {
+        $jenis = 'KDP';
+        } elseif ($jumlahKarakter == 7) {
+        $jenis = 'SPV';
+        } elseif ($jumlahKarakter == 9) {
+        $jenis = 'LDR/OPR';
+        } elseif ($jumlahKarakter == 2 || $jumlahKarakter == 3) {
+        $jenis = 'GMR';
+        } else {
+        $jenis = 'Jenis tidak dikenali'; // Atur jenis untuk kondisi lainnya
+        }
+        } else {
+        // Handle the case where no results are returned
+        $jenis = 'Jenis tidak dikenali';
+        }
+
+        $cleanedStringDept = str_replace(' ', '', $userInfo[0]->descr);
+        $cleanedStringDeptFinal = substr($cleanedStringDept, 0, 3);
+        $userInfoOccupation = $jenis;
+        $userInfoDept = $cleanedStringDeptFinal;
+        @endphp
+        @if ($userInfoOccupation == 'GMR' or $userInfoOccupation == 'KDP' or $userInfoDept == 'HRD')
         <ul class="sidebar-menu">
             <li class="dropdown">
                 <a href="#" class="nav-link has-dropdown {{ request()->is('dailyattendance*')||request()->is('weeklyattendance*')||request()->is('monthlyattendance*') ? 'text-primary' : '' }}"><i class="fas fa-th"></i>
@@ -15,13 +63,24 @@
                         <a class="nav-link {{ request()->is('dailyattendance*') ? 'text-primary' : '' }}" href="/dailyattendance">Daily attendance</a>
                     </li>
                     <li>
-                        <a class="nav-link {{ request()->is('monthlyattendance') ? 'text-primary' : '' }}" href="/monthlyattendance">Monthly attendance</a>
+                        <a class="nav-link {{ request()->is('monthlyattendance*') ? 'text-primary' : '' }}" href="/monthlyattendance">Monthly attendance</a>
                     </li>
                     <li>
                         <a class="nav-link {{ request()->is('historyattendance*') ? 'text-primary' : '' }}" href="/historyattendance">History attendance</a>
                     </li>
+                </ul>
+                <a class="sidebar-menu" href="#"><i class="fas fa-edit"></i><span>Cuzia<span class="text-sm badge bg-secondary" style="width: 100px;">Coming Soon</span></span></a>
             </li>
         </ul>
-        <a class="sidebar-menu" href="#"><i class="fas fa-edit"></i><span>Cuzia<span class="text-sm badge bg-secondary" style="width: 100px;">Coming Soon</span></span></a>
+        <!-- ... Menu lainnya untuk GMR, KDP, dan HRD ... -->
+        @else
+        <ul class="sidebar-menu">
+            <li>
+                <a class="nav-link {{ request()->is('historyattendance*') ? 'text-primary' : '' }}" href="/historyattendance">History attendance</a>
+                <a class="sidebar-menu" href="#"><i class="fas fa-edit"></i><span>Cuzia<span class="text-sm badge bg-secondary" style="width: 100px;">Coming Soon</span></span></a>
+            </li>
+        </ul>
+        @endif
+
     </aside>
 </div>
