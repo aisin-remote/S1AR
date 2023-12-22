efd85cf7792afd0f9029b60f81d89ff311e1527a
naufalamr17 <naufal.amr17@outlook.co.id>
2023-12-22 07:26:28 +0700

    Update: Add new table

--- siar-mysql.sql
+++ siar-mysql.sql
@@ -1,11 +1,22 @@
-USE `siar-mysql`;
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               5.5.67-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             10.2.0.5599
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 -- Dumping structure for table siar-mysql.employee
 CREATE TABLE IF NOT EXISTS `employee` (
   `coid` varchar(150) DEFAULT NULL,
   `empno` varchar(150) DEFAULT NULL,
   `empnm` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -14,7 +25,7 @@ CREATE TABLE IF NOT EXISTS `hirarki` (
   `empno` varchar(150) DEFAULT NULL,
   `hirar` varchar(150) DEFAULT NULL,
   `mutdt` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -22,7 +33,17 @@ CREATE TABLE IF NOT EXISTS `hirarki` (
 CREATE TABLE IF NOT EXISTS `hirarkidesc` (
   `hirar` varchar(150) DEFAULT NULL,
   `descr` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table siar-mysql.holiday
+CREATE TABLE IF NOT EXISTS `holiday` (
+  `date` varchar(50) DEFAULT NULL,
+  `note` varchar(50) DEFAULT NULL,
+  `updated_at` varchar(50) DEFAULT NULL,
+  `created_at` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -35,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `kehadiran1` (
   `timot` varchar(150) DEFAULT NULL,
   `crtdt` varchar(150) DEFAULT NULL,
   `lupddt` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -47,29 +68,29 @@ CREATE TABLE IF NOT EXISTS `kehadiran2` (
   `rsccd` varchar(150) DEFAULT NULL,
   `crtdt` varchar(150) DEFAULT NULL,
   `lupddt` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table siar-mysql.kehadiranmu
 CREATE TABLE IF NOT EXISTS `kehadiranmu` (
-  `coid` varchar(150),
-  `empno` varchar(150),
-  `schdt` varchar(150),
-  `rsccd` varchar(150),
+  `coid` varchar(150) DEFAULT NULL,
+  `empno` varchar(150) DEFAULT NULL,
+  `schdt` varchar(150) DEFAULT NULL,
+  `rsccd` varchar(150) DEFAULT NULL,
   `crtdt` varchar(150) DEFAULT NULL,
   `lupddt` varchar(150) DEFAULT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
 -- Dumping structure for table siar-mysql.migrations
 CREATE TABLE IF NOT EXISTS `migrations` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `migration` varchar(255) NOT NULL,
-  `batch` int NOT NULL,
+  `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- Data exporting was unselected.
 
@@ -83,5 +104,10 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `users_npk_unique` (`npk`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
