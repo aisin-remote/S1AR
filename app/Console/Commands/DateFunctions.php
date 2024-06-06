9b6c958d5fc435fe772410bcd588fffd9e912dfd
sonya88 <sonyasinaga071003@gmail.com>
2024-06-06 11:15:17 +0700

    date funtions

--- app/Console/Commands/DateFunctions.php
+++ app/Console/Commands/DateFunctions.php
@@ -3,6 +3,6 @@
 use Carbon\Carbon;
 
 function getCurrentDate() {
-    // return Carbon::now()->format('Y-m-d');
-    return '2022-09-22';
+    return Carbon::now()->format('Y-m-d');
+    // return '2022-09-22';
 }
